a054461a900dcfbe3a01a242f00e6e2605ad9a82
ryan <ryan.williams93@aol.com>
2022-02-07 14:03:31 -0600

    checkpoint: models/tag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,14 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_id: {},
+    tag_id: {}
   },
   {
     sequelize,
